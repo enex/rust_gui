8be85ff4375871cf5c4ed99a33146912a2622aaa
enex <sive@web.de>
2015-02-01 17:17:29 +0100

    temporary remove other components

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,28 +1,26 @@
 //! Some basic components for the day to day use
 //! but they are not jet all ready, so choose only components marked as stable for productive use.
 
-pub use self::layout::{Row, Col, Layout};
+//pub use self::layout::{Row, Col, Layout};
 
-/*
 pub use self::button::{Button, ButtonEvent};
 
 pub use self::label::Label;
 
-pub use self::image::Icon;
+//pub use self::image::Icon;
 
-pub use self::slider::{Slider, SliderEvent};
+//pub use self::slider::{Slider, SliderEvent};
 
-pub use self::checkbox::{Checkbox, CheckboxEvent};
+//pub use self::checkbox::{Checkbox, CheckboxEvent};
 
-pub use self::text_input::{TextInput, TextInputEvent};*/
+//pub use self::text_input::{TextInput, TextInputEvent};
 
-/*
-pub mod image;
+//pub mod image;
+//pub mod text_input;
+//pub mod tabs;
+
+//pub mod slider;
+//pub mod checkbox;
+//pub mod layout;
 pub mod button;
 pub mod label;
-pub mod text_input;
-pub mod tabs;
-
-pub mod slider;
-pub mod checkbox;*/
-pub mod layout;
