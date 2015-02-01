f03119a6b49b43c5c9bb09de16e44094ec46abe2
enex <sive@web.de>
2015-02-01 17:15:24 +0100

    add builder pattern with macros

--- src/components/button.rs
+++ src/components/button.rs
@@ -1,24 +1,24 @@
 use Widget;
-use CTX;
+use Context;
 use components::Label;
 use Event;
 
-#[deriving(Show, Copy)]
+#[derive(Show, Copy)]
 pub enum ButtonEvent{
     Click,//click event on mouse down
     Hover,//if mose move and mouse over button
     Leave
 }
 
-#[deriving(Clone, Show)]
-pub struct Button{
-    pub text: String,
+#[derive(Clone, Show)]
+pub struct Button<'a>{
+    pub text: &'a str,
     pub width: f64,
     pub height: f64,
 }
 
-impl Button{
-    pub fn new(text: String, width: f64, height: f64) -> Button{
+impl<'a> Button<'a>{
+    pub fn new(text: &'a str, width: f64, height: f64) -> Button<'a>{
         Button{
             text: text,
             width: width,
@@ -27,9 +27,56 @@ impl Button{
     }
 }
 
-impl Widget<ButtonEvent> for Button{
-    fn render(&mut self, ctx: &mut CTX<ButtonEvent>) -> (f64, f64) {
-        let mut hover = false;
+#[macro_export]
+macro_rules! widget{
+    ( $name:ident ( $( $atr_name:ident : $attr_type:ty),* ){
+        $( $prop_name:ident : $prop_type:ty = $prop_val:expr ),*
+    }) => (
+        pub struct $name{
+            $( $prop_name : $prop_type ,)*
+        }
+        impl $name{
+            pub fn new( $( $atr_name : $attr_type),* ) -> $name{
+                $name{
+                    $( $prop_name : $prop_val ),*
+                }
+            }
+            //TODO: implement setter
+        }
+    )
+}
+
+widget!(Box(width: f64, height: f64){
+    width: f64 = width,
+    height: f64 = height,
+    color: (f32, f32, f32) = (0.0,0.0,0.0)
+});
+
+macro_rules! setter{
+    (
+        $e:ty,
+        $( $prop:ident : $prop_type:ty ),+
+    ) => (
+        impl<'a> $e{
+            $(
+                pub fn $prop(&'a mut self, $prop: $prop_type) -> &'a mut $e{
+                    self.$prop = $prop;
+                    self
+                }
+            )+
+        }
+    )
+}
+
+setter!(Button<'a>,
+    width: f64,
+    height: f64,
+    text: &'a str
+);
+
+impl<'a> Widget for Button<'a>{
+    fn render(&self, ctx: &mut Context) {
+        /*let mut hover = false;
         ctx.mouseover((self.width, self.height), |event, ctx|{
             //println!("button event: {}", event);
             hover = true;
@@ -42,23 +89,20 @@ impl Widget<ButtonEvent> for Button{
                 },
                 _ => {}
             }
-        });
+        });*/
         ctx.draw(|c|{
-            if hover{
+            /*if hover{
                 c.set_source_rgb(0.55, 0.55, 0.95);
-            }else{
+            }else{*/
                 c.set_source_rgb(0.27, 0.36, 0.93);
-            }
+            /*}*/
             c.rectangle(0.0, 0.0, self.width, self.height);
             c.fill();
             c.stroke();
         });
-        ctx.go_to(0.0,0.0);
-        let mut l = Label::new(self.text.clone());
-        l.font_size = self.height - 4.0;
-        ctx.go_to(3.0, 0.0);
-        ctx.add(1, &mut l, None);
-        (self.width, self.height)
+        //ctx.go_to(0.0,0.0);
+        //ctx.go_to(3.0, 0.0);
+        ctx.add(1, Label::new(self.text.clone()).font_size(self.height - 4.0))
     }
     fn size(&self) -> (f64, f64) {
         (self.width, self.height)
--- src/components/label.rs
+++ src/components/label.rs
@@ -1,37 +1,66 @@
 use Widget;
-use CTX;
+use Context;
 use cairo::ffi;
 
-pub struct Label{
-    pub text: String,
+pub struct Label<'a>{
+    pub text: &'a str,
     pub font_size: f64,
+    //TODO: add other text styling options
+    pub font_face: &'a str,
 }
 
-impl Label{
-    pub fn new(text: String) -> Label{
+impl<'a> Label<'a>{
+    pub fn new(text: &'a str) -> Label<'a>{
         Label{
             text: text,
             font_size: 20.0,
+            font_face: "Sans",
         }
     }
 }
 
-impl Widget<()> for Label{
-    fn render(&mut self, ctx: &mut CTX<()>) -> (f64, f64) {
+macro_rules! setter{
+    (
+        $e:ty,
+        $( $prop:ident : $prop_type:ty ),+
+    ) => (
+        impl<'a> $e{
+            $(
+                pub fn $prop(&'a mut self, $prop: $prop_type) -> &'a mut $e{
+                    self.$prop = $prop;
+                    self
+                }
+            )+
+        }
+    )
+}
+
+///implement setter for the propertys
+setter!(Label<'a>,
+    text: &'a str,
+    font_size: f64,
+    font_face: &'a str
+);
+
+pub enum LabelEvent{
+    Hover
+}
+impl<'a> Widget for Label<'a>{
+    //type Event = LabelEvent;
+
+    fn render(&self, ctx: &mut Context) {
         ctx.draw(|c|{ //right now the cairo wrapper does not offer an abstract way for text rendering
             unsafe{
+                use std::ffi::CString;
+
                 let cr = c.cairo_ptr();
                 ffi::cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
-                ffi::cairo_select_font_face(cr, "Sans".to_c_str().as_ptr(), ffi::CAIRO_FONT_SLANT_NORMAL, ffi::CAIRO_FONT_WEIGHT_NORMAL);
+                ffi::cairo_select_font_face(cr, CString::from_slice(self.font_face.as_bytes()).as_ptr(), ffi::CAIRO_FONT_SLANT_NORMAL, ffi::CAIRO_FONT_WEIGHT_NORMAL);
                 ffi::cairo_set_font_size(cr, self.font_size);
 
                 ffi::cairo_move_to(cr, 0.0, self.font_size);
-                ffi::cairo_show_text(cr, self.text.to_c_str().as_ptr());
+                ffi::cairo_show_text(cr, CString::from_slice(self.text.as_bytes()).as_ptr());
             }
         });
-        (0.0,0.0)
-    }
-    fn size(&self) -> (f64, f64) {
-        (0.0, 0.0)
     }
 }
