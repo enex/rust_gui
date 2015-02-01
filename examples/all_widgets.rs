f71bc43cdb5ac6590e76deef345ef78d10b2d624
enex <sive@web.de>
2015-02-01 17:14:37 +0100

    add button and defined a macro to easily make gui work

--- examples/all_widgets.rs
+++ examples/all_widgets.rs
@@ -1,29 +1,29 @@
 extern crate gui;
 
+use gui::components::{Button};
+//TODO: insert widgets here
+
+///a macro to call ctx.add function more convinient and with less writing
+macro_rules! gui{
+    ($target:ident ,
+        $(
+            $name:ident ( $($arg_val:expr),* )
+            $( . $prop_name ( $( $prop_arg_val ),* ) )*
+        ),+
+    ) => ({
+        $({
+            $target.add(
+                $name::new($($arg_val),*)
+                $(.$prop_name($($prop_arg_val),*))* ,
+                None
+            );
+        })+
+    })
+}
+
 fn main(){
-    gui::Window::new("test",640,480).show(|ctx| ctx.draw(|cr|{
-        cr.set_source_rgb(1.0, 1.0, 1.0);
-        cr.paint();
-        cr.set_source_rgb(1.0, 0.0, 0.0);
-        cr.set_line_width(5.0);
-        cr.move_to(5.0, 5.0);
-        cr.line_to(15.0, 5.0);
-        cr.line_to(15.0, 15.0);
-        cr.line_to(5.0, 15.0);
-        cr.line_to(5.0,5.0);
-        cr.set_line_width(0.2);
-        cr.stroke();
-
-        cr.rectangle(50.0, 50.0, 20.0, 20.0);
-        cr.set_source_rgba(1.0, 0.0, 0.0, 0.80);
-        cr.fill();
-
-        cr.rectangle(100.0, 10.0, 40.0, 40.0);
-        cr.set_source_rgba(0.0, 1.0, 0.0, 0.60);
-        cr.fill();
-
-        cr.rectangle(100.0, 50.0, 50.0, 50.0);
-        cr.set_source_rgba(0.0, 0.0, 1.0, 0.40);
-        cr.fill();
-    }));
+    gui::Window::new("test",640,480).show(|ctx|{
+        ctx.add(1, &Button::new("test-Button",270.0,50.0));
+    });
+    //gui::Window::new("test2", 640, 480).show(|ctx| ctx.add());
 }
