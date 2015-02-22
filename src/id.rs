1b5bb1ffd929d67ebf6b6add385454253a28c345
enex <sive@web.de>
2015-02-22 17:19:51 +0100

    remove unused files

--- src/id.rs
+++
@@ -1,8 +0,0 @@
-/// The id-type used internaly to identify each widget.
-/// it is an array consistiong of 12 ids of the elements an their children
-/// if the id == 0, the element has no id assigned. Thats why ids should start
-/// from 1
-pub type ID = [u16;12];
-
-///The null id
-pub const NULL_ID: ID = [0,0,0,0,0,0,0,0,0,0,0,0];
--- src/testing.rs
+++
@@ -1,6 +0,0 @@
-use Context;
-
-///This is a testing context which can be used to test Widgets
-pub struct CTX{
-    pub children: Vec<Widget>
-}
